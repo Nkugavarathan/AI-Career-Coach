55f0e3e7fe4ebb5d4006f838b2ae063e50dae44e
Nkugavarathan <kugavarathan28@gmail.com>
2025-11-15 08:57:45 +0530

    innegest package installed

--- components/Header.jsx
+++ components/Header.jsx
@@ -90,7 +90,7 @@ export default async function Header() {
         {/* login */}
         <SignedOut>
           <SignInButton>
-            <button className=" text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            <button className="bg-black/70 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
               Sign In
             </button>
           </SignInButton>
